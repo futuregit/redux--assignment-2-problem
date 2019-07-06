6b27ea3d0a7092ca28a93c4f5c26e44ec2b4a144
futuregit <gmail.com>
2019-07-06 17:54:44 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import reducer from './store/reducer';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  persons: ['hey']
+};
+
+const reducer = (state=initialState, action) => {
+  return state;
+}
+
+export default reducer;
